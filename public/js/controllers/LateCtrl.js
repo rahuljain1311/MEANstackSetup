54b293e354e719b72a74a22f2e8610431d5e0e16
Rahul <rahul_jain@mckinsey.com>
2018-01-06 13:16:43 +0530

    Add date and brs

--- public/js/controllers/LateCtrl.js
+++ public/js/controllers/LateCtrl.js
@@ -1,5 +1,7 @@
-angular.module('LateCtrl', []).controller('LateController', function($scope, $http) {
+angular.module('LateCtrl', []).controller('LateController', function($scope, $http, $filter) {
 
 	$scope.tagline = 'Nothing beats a pocket protector!';
 
+	var date = new Date();
+	$scope.date = $filter('date')(new Date(), 'dd/MM/yyyy');
 });
