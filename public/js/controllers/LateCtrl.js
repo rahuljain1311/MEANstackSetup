6a3c7b09d3eac5a360265f105a59aa133c74bec9
Rahul <rahul_jain@mckinsey.com>
2018-01-06 22:02:01 +0530

    Add Moment in FE coz of high unreliablity

--- public/js/controllers/LateCtrl.js
+++ public/js/controllers/LateCtrl.js
@@ -1,16 +1,17 @@
-angular.module('LateCtrl', []).controller('LateController', function($scope, $http) {
+
+angular.module('LateCtrl', []).controller('LateController', function($scope, $http, moment) {
 
 	$scope.tagline = 'Nothing beats a pocket protector!';
 
-	var date = new Date();
-	$scope.date = new Date();
+	// var date = new Date();
+	$scope.date = new moment();
 
 	$http.get('/api/users').then(function(response){ $scope.names = response.data; });
 
 	$scope.markLate = function (id) {
 		
 		const parameters = JSON.stringify({
-			date: $scope.userdate,
+			date: moment($scope.userdate).format(),
 			isLate: true
 		})
 		return $http.put('/api/user/'+ id, parameters) // Send date and boolean isLate = true
@@ -22,7 +23,7 @@ angular.module('LateCtrl', []).controller('LateController', function($scope, $ht
 	$scope.markEarly = function (id) {
 		
 		const parameters = JSON.stringify({
-			date: $scope.userdate,
+			date: moment($scope.userdate).format(),
 			isLate: false
 		})
 		return $http.put('/api/user/'+ id, parameters) // Send date and boolean isLate = false
